59c40446221aef18659ccb25676e65e9f0f88bb5
Riguy1122 <rileymgross@gmail.com>
2026-06-07 23:06:16 -0700

    Implemented system stats update in MainWindow, UtilityLogic updated

--- BubbaMain/MainWindow.py
+++ BubbaMain/MainWindow.py
@@ -1,6 +1,8 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
+from PySide6.QtCore import QTimer
 from moods import mood_list
+from UtilityLogic import get_all_stats
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -15,13 +17,29 @@ class MainWindow(QWidget):
 
         self.button = QPushButton("Next mood")
         self.label = QLabel(mood_list[self.current_mood]["message"])
+        self.label2 = QLabel("getting stats...")
         layout = QVBoxLayout()
 
         self.button.clicked.connect(self.cycle_mood)
 
         layout.addWidget(self.label)
         layout.addWidget(self.button)
+        layout.addWidget(self.label2)
         self.setLayout(layout)
+
+        self.timer = QTimer()
+        self.timer.timeout.connect((self.update_stats))
+        self.timer.start(500)
+
+
+
+    def update_stats(self):
+        self.stats_data = get_all_stats()
+        self.cpu = self.stats_data["cpu"]["percent"]
+        self.ram = self.stats_data["memory"]["used"]
+        self.battery = self.stats_data["battery"]["percent"]
+        self.text = f"CPU: {self.cpu}%\nRAM: {self.ram}\nBattery: {self.battery}%"
+        self.label2.setText(self.text)
 
     def cycle_mood(self):
         self.current_mood = (self.current_mood + 1) % len(mood_list)
@@ -29,6 +47,7 @@ class MainWindow(QWidget):
 
 
 if __name__ == "__main__":
+    print(get_all_stats())
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
--- BubbaMain/UtilityLogic.py
+++ BubbaMain/UtilityLogic.py
@@ -1,39 +1,58 @@
 import psutil
 
-#index 0 returns percent used, index 1 returns the amount of cpus on the host.(probably not useful)
-#ex: cpu = get_cpu_stats()
-#    print(cpu[0])
-
-def get_cpu_stats():
-    cpu_pcent = psutil.cpu_percent(1)
-    cpu_num = psutil.cpu_count(False)
-    cpu_info = [cpu_pcent, cpu_num]
+def get_cpu_stats(): #cpu info
+    cpu_pcent = psutil.cpu_percent(interval=None)
+    cpu_cores = psutil.cpu_count(False)
+    cpu_info = {
+        "percent": cpu_pcent,
+        "cores": cpu_cores
+    }
     return cpu_info
 
-#index 0 returns memory used, index 1 returns memory available.
-#ex: memory = get_mem_stats()
-#    print(memory[0])
+
 
 def get_mem_stats():
     mem = psutil.virtual_memory() #RAM
     mem_usedGiB = round(mem.used/1073741824, 2)
     mem_availGiB = round(mem.available/1073741824, 2)
-    mem_info = [mem_usedGiB, mem_availGiB]
+    mem_info = {
+        "used": mem_usedGiB,
+        "available": mem_availGiB
+    }
     return mem_info
 
-#index 0 returns battery percentage, index 1 returns battery time left in hours, index 2 returns battery time left in seconds.
-#ex: batt = get_batt_stats()
-#    print(batt[0])
 
-def get_batt_stats():
+def get_batt_stats(): #battery info
     battery = psutil.sensors_battery()
-    if battery is None:
-        return [None, None, None]
+    if battery is None: #in case of no battery
+        return {"percent": None, "hours": None, "seconds": None, "plugged_in": None}
     battery_pcent = battery.percent
+    is_plugged_in = battery.power_plugged
     battery_time_secs = battery.secsleft
-    battery_time_hours = battery_time_secs / 60
-    batt_info = [battery_pcent, battery_time_hours, battery_time_secs]
+    battery_time_hours = battery_time_secs / 3600
+    batt_info = {
+        "percent": battery_pcent,
+        "hours": battery_time_hours,
+        "seconds": battery_time_secs,
+        "plugged_in": is_plugged_in
+    }
     return batt_info
+
+#searches all stats, can return a summary of all sections, a summary of just one
+#or a specific stat within one section
+#ex: statistics = get_all_stats()
+#statistics = get_all_stats()
+#print(statistics["cpu"]["cores"])
+
+def get_all_stats():
+    stats = {
+        "cpu": get_cpu_stats(),
+        "memory": get_mem_stats(),
+        "battery": get_batt_stats()
+    }
+    return stats
+
+
 
 
 
